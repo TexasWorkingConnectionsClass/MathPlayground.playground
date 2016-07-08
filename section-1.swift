0438bdd66793ac116dff8abdbb8b7ae0debe34be
Bill Skrzypczak <bill@webbuildersconsulting.com>
2016-07-08 15:06:08 -0500

    MP

---
+++ section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Print some text
+var str = "Welcome To Math Playground"
+
+// A simple For loop
+for x in 0..<21{
+    let t = Double(x) * sin(Double(x) * M_PI_4/4)
+}
+
+
+
+
+
+
+
